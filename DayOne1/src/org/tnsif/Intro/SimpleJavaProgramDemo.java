34a2dbee5d370b45f1876b923bc8b64736380f67
pratikG58 <pratiksunilgosavi@gmail.com>
2023-02-09 20:31:08 +0530

    First Commit

---
+++ DayOne1/src/org/tnsif/Intro/SimpleJavaProgramDemo.java
@@ -0,0 +1,13 @@
+package org.tnsif.Intro;
+
+//simple java program to print a line
+public class SimpleJavaProgramDemo {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Java Fullstack Program");
+		
+
+	
+	}
+
+}
