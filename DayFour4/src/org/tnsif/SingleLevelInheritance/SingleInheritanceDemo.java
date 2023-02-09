7c593c89f5e5c147541fe38048e3dd48af7c4c66
pratikG58 <pratiksunilgosavi@gmail.com>
2023-02-09 20:32:37 +0530

    First Commit

---
+++ DayFour4/src/org/tnsif/SingleLevelInheritance/SingleInheritanceDemo.java
@@ -0,0 +1,12 @@
+package org.tnsif.SingleLevelInheritance;
+
+public class SingleInheritanceDemo {
+
+	public static void main(String[] args) {
+		Student s=new Student();
+		Student s1=new Student("Pratik","345612349873","Pune",98984567L,101,"Pratik Gosavi");
+		System.out.println(s1);
+	}
+	
+
+}
