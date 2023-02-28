9266c663c1a7b22a6cf1e0cc18c1bf5224347d32
pratikG58 <pratiksunilgosavi@gmail.com>
2023-03-01 00:06:17 +0530

    First commit

---
+++ MoneyMoneyBankApp_PratikGosavi/src/org/tnsif/client/Client.java
@@ -0,0 +1,22 @@
+package org.tnsif.client;
+
+import org.tnsif.Application.MMBankFactory;
+import org.tnsif.Application.MMCurrentAcc;
+import org.tnsif.Application.MMSavingAcc;
+import org.tnsif.framework.BankFactory;
+
+public class Client {
+	public static void main(String args[])
+	{
+		BankFactory b=new MMBankFactory();
+		MMCurrentAcc c=new MMCurrentAcc(102,"Pratik",65000.0f,60.0f);
+		MMSavingAcc s=new MMSavingAcc(103,"Payal",10000.0f,true);
+		System.out.println("Saving Account");
+		System.out.println(s);
+		s.withdraw(s.getAccBal());
+		System.out.println("Current Account");
+		System.out.println(c);
+		c.withdraw(c.getAccBal());
+		
+	}
+}
